3516a0582773b3923e3f0dcbef8a68b27e257664
Karsten Schmidt <k@postspectacular.com>
2014-10-13 16:37:57 +0100

    add type arg for alerts widget

--- src-cljs/imago/alerts.cljs
+++ src-cljs/imago/alerts.cljs
@@ -1,14 +1,16 @@
 (ns imago.alerts
   (:require-macros
-   [cljs.core.async.macros :refer [go go-loop]])
+   [cljs.core.async.macros :refer [go]])
   (:require 
    [thi.ng.cljs.log :refer [debug info warn]]
    [thi.ng.cljs.dom :as dom]
    [cljs.core.async :as async :refer [<! timeout]]))
 
 (defn alert
-  [msg parent]
-  (let [alert (dom/create-dom! [:div.alert.alert-danger msg] nil)]
+  [type msg parent]
+  (let [alert (dom/create-dom!
+               [:div {:class (str "alert alert-" (name type))} msg]
+               nil)]
     (dom/insert! alert parent)
     (go
       (<! (timeout 3000))
